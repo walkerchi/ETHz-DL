3c9da632d9f87bbf80a0769d90a7d51d1afa8fec
largraf <lars.graf12@gmail.com>
2022-12-16 14:32:48 +0100

    finished fisher pruning loading

--- datasets/mscoco/mscoco.py
+++ datasets/mscoco/mscoco.py
@@ -13,10 +13,10 @@ IMGS_DIR = str(pathlib.Path(__file__).parent.resolve() / "val2017")
 
 
 class MSCOCO:
-    def __init__(self, 
+    def __init__(self,
                 n_samples:Optional[int]=None,
                 layout:str =  "image-caption[0]",
-                image_path:str=IMGS_DIR, 
+                image_path:str=IMGS_DIR,
                 ann_path:str=CAPS_PATH):
         """
             there are 5000 images and 4~6 captions for each image. (25014 captions)
@@ -27,14 +27,14 @@ class MSCOCO:
                             if not None, it will do a lazy loading to limit the memory cost and loading time
                 layout:     str, default `image-caption[0]`
                             `caption`:  the image is copied for each corresponding caption
-                                        which means the total length will be 20,000 ~ 30,000       
+                                        which means the total length will be 20,000 ~ 30,000
                             `image-caption[i]`: get the i-th caption for each image,
                                         which means the total length will be 5000,
                                         the `image-caption[0]` is getting the first caption for each image.
                 image_path: str
-                            the path to coco image folder 
+                            the path to coco image folder
                 ann_path:   str
-                            the path to annotation json file 
+                            the path to annotation json file
         """
         assert os.path.isdir(image_path), f"The image_path should be directory"
         assert ann_path.endswith('.json'), f"The ann_path should ends with `.json`"
@@ -43,7 +43,7 @@ class MSCOCO:
             ann = json.load(f)
 
         imageid2filename = {item['id']:item['file_name'] for item in ann['images']}
-        
+
         if layout == "caption":
             if n_samples is not None:
                 assert n_samples <= len(ann['annotations']), f"the number of samples should not be greater than the total length"
@@ -55,7 +55,7 @@ class MSCOCO:
                 item = ann['annotations'][i]
                 images.append(imageid2filename[item['image_id']])
                 captions.append(item['caption'])
-            
+
         else:
             caption_index = int(re.findall('-?\d',layout)[0])
             if n_samples is not None:
--- models/huggingface_pruned_clip/__init__.py
+++ models/huggingface_pruned_clip/__init__.py
@@ -2,19 +2,18 @@ import os
 import torch
 from importlib import import_module
 from ..huggingface_clip import HuggingFaceCLIP
-from .modeling_pruned_clip import CLIPModel as CLIPModel_p
-import modeling_pruned_clip
+from models.huggingface_pruned_clip import modeling_pruned_clip
+from .modeling_pruned_clip import CLIPModel as CLIPModel_pruned
+import sys
+# Next line is necessary for unpickling (in torch.load) to work
+sys.modules['modeling_pruned_clip'] = modeling_pruned_clip
 CACHE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),".cache")
 
 
 class HuggingFacePrunedCLIP(HuggingFaceCLIP):
     def __init__(self, model_str:str, pruning_version:str='v0', cache_dir:str=CACHE_DIR):
         super().__init__(model_str, cache_dir)
-        # path = '.' + 'huggingface_pruned_clip' + '.' +  pruning_version
-        # pruned = import_module(path, 'models')
-        # self.model           = pruned.CLIPModel_pruned.from_pretrained(model_str, cache_dir=cache_dir)
         self.model = torch.load(f'models/huggingface_pruned_clip/pruned_models/{pruning_version}')
-        breakpoint()
         self.pruning_version = pruning_version
 
     @property
