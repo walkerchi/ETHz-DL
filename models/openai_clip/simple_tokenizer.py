2815e062d165fba125003b1c157d13d42f0c7cdf
walkerchi <walkerchi@foxmail.com>
2022-12-18 23:21:37 +0100

    add LICENSE

--- models/openai_clip/simple_tokenizer.py
+++ models/openai_clip/simple_tokenizer.py
@@ -1,3 +1,19 @@
+# THIS FILE IS PART OF OpenAI CLIP 
+# simple_tokenizer.py - The toeknize frontend of the CLIP model 
+# 
+# THIS PROGRAM IS FREE SOFTWARE, IS LICENSED UNDER MIT LICENSE
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# Copyright (c) 2021 OpenAI
+
 import gzip
 import html
 import os
